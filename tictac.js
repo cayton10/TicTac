e7cadce48e9627aaa66a45d70ea9431306b02056
Obi-Wan <cayton10@live.marshall.edu>
2020-02-19 11:35:17 -0500

    responsive board

--- tictac.js
+++ tictac.js
@@ -1,5 +1,15 @@
 AOS.init({
     once: true
-    
-   
   })
+
+  $(document).ready(function()
+  {
+    
+    /*LIFTED THIS FROM https://stackoverflow.com/questions/30139622/how-to-set-a-divs-height-equal-to-that-divs-width*/
+    //GRABS width of .square class divs and applies that value to height to maintain apsect ratio. 
+
+    /*$('.square').height(function(){
+      return $(this).width();
+    });*/
+
+  });
