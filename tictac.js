d1aba6b017c0eae87ae614aabdcb6e1cb43eeef9
Obi-Wan <cayton10@live.marshall.edu>
2020-02-19 11:45:15 -0500

    change responsive squares

--- tictac.js
+++ tictac.js
@@ -8,8 +8,8 @@ AOS.init({
     /*LIFTED THIS FROM https://stackoverflow.com/questions/30139622/how-to-set-a-divs-height-equal-to-that-divs-width*/
     //GRABS width of .square class divs and applies that value to height to maintain apsect ratio. 
 
-    /*$('.square').height(function(){
+    $('.square').height(function(){
       return $(this).width();
-    });*/
+    });
 
   });
