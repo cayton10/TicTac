a26986175e68740266f5f26e8851f52da8ac9ffb
Obi-Wan <cayton10@live.marshall.edu>
2020-02-21 16:10:49 -0500

    store computer choices in array

--- tictac.js
+++ tictac.js
@@ -4,12 +4,14 @@ AOS.init({
     once: true
   });
 
-/* ---------------------------- ON DOCUMENT LOAD ---------------------------- */
+/* -------------------------------------------------------------------------- */
+/*                              ON DOCUMENT LOAD                              */
+/* -------------------------------------------------------------------------- */
 
   $(document).ready(function()
   {
 
-/* ------------------------- ANIMATIONS ON PAGE LOAD ------------------------ */
+/* ------------------------- animation on page load ------------------------- */
 
     $(".container-fluid").hide(0).fadeIn(1200);
 
@@ -19,8 +21,69 @@ AOS.init({
 /* --------------------------- mouse over squares --------------------------- */
 
 
-/* ---------------------------- ON CLICK ADD PLAY --------------------------- */
-    $('.square').on(click, )
 
+
+    // check in all 8 directions, +10
+    // play against AI, +20
+    // make it to where I as a player cannot beat the AI, +10
+
+/* ---------------------------- declare variables --------------------------- */
+var winCondition = [ [0,1,2], [3,4,5], [6,7,8] ];
+var userChoices = [];
+var computerChoices = [];
+var countTurn = 0;
+    /* ----------------------------- register clicks ---------------------------- */
+
+    $('.square').click(function()
+    {
+
+        // IF this square is empty...
+        if ($(this).html() == "")  // checks to see if what you clicked on was blank < -- thanks
+        {
+            //player turn
+                $(this).html("<img class='img-fluid' src='x.png' />");
+                $(this).addClass('x');
+                //APPEND USER CHOICE TO USERCHOICES ARRAY
+                userChoices.push($('.square').index($(this)));
+                console.log(userChoices);
+                countTurn ++;
+                console.log(countTurn);
+
+                //check winner
+                  if(countTurn > 2) 
+                  {
+
+                  }
+                  console.log(computerChoices);
+            //player turn
+                computerChoice();
+                
+                
+        }
+    });
 
   });
+
+
+/* -------------------- COMPUTER RANDOM SQUARE SELECTION -------------------- */
+
+  function computerChoice (){
+
+    countTurn = 0;
+    computerChoices = [];
+    //CHOICE = jQuery array of .squares without class 'x'.
+    let choice = $(".square:not(.x,.o)");
+    //computer choice =  
+    let compChoice = choice[Math.floor(Math.random() * choice.length)];
+
+    console.log(compChoice);
+    //add 'chosen' class to div element
+    $(compChoice).addClass('o');
+
+    $(compChoice).html("<img class='img-fluid' src='o.png' />");
+    computerChoices = ($(compChoice).index('.square'));
+    return computerChoices
+    console.log(computerChoices);
+    countTurn++;
+    console.log(countTurn)
+  };
